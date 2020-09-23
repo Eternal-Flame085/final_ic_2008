032f2f260f57efcfe5fc2af678031d2e285943da
Roberto Basulto <roberto.basulto085@gmail.com>
2020-09-23 11:45:34 -0400

    cookbook exists

---
+++ test/cook_book_test.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+require "minitest/pride"
+require './lib/pantry'
+require './lib/ingredient'
+require './lib/recipe'
+require './lib/cook_book'
+
+
+class CookBookTest < Minitest::Test
+	def test_it_exist
+    cookbook = CookBook.new
+
+    assert_instance_of CookBook, cookbook
+	end
+end
