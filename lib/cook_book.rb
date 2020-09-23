f9e03ab5f8731b9c42c0c2eee210586e0ab2a107
Roberto Basulto <roberto.basulto085@gmail.com>
2020-09-23 12:13:12 -0400

    created ingredients method and test for cookbook

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -7,4 +7,14 @@ class CookBook
   def add_recipe(recipe)
     @recipes << recipe
   end
+
+  def ingredients
+    ingredient_list = []
+    @recipes.each do |recipe|
+      ingredient_list << recipe.ingredients.map do |ingredient|
+        ingredient.name
+      end
+    end
+    ingredient_list.flatten!.uniq
+  end
 end
